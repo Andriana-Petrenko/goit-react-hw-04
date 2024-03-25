74dadc00603bdc15f35e73ef50659d6c42aa797f
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-03-26 00:33:11 +0200

    add components

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,8 @@
+
+const LoadMOreBtn = () => {
+  return (
+    <button type="button">Load more</button>
+  )
+}
+
+export default LoadMOreBtn
--- src/components/SearchBox/SearchBox.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./SearchBox.module.css"
-const SearchBox = ({filter,onChangeFilter}) => {
-    return (
-        <label className={css.filter}>Find contacts by name
-            <input type="text" value={filter} onChange={onChangeFilter}/></label> 
-  )
-}
-
-export default SearchBox
