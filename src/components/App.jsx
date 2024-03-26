ca1e375c1addc0f6d7dda007c0bad91bd0c92860
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-03-26 12:17:16 +0200

    add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from "react";
 import SearchBar from './SearchBar/SearchBar.jsx'
 
 const App = () => {
-  
+  const onSubmit = (inputSearch) => {
+  console.log(inputSearch);
+}
+
   return (
     <div>
-      <SearchBar onSubmit={onSubmit} />
+      <SearchBar onSubmit ={onSubmit}/>
     </div>
   );
 };
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,24 @@
+import toast, { Toaster } from 'react-hot-toast';
+const SearchBar = ({ onSubmit }) => {
+    
+const handleOnSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const inputSearch = form.elements.search.value;
+          if (inputSearch.trim() === "") {
+              toast.error('Please enter search term!');
+              return;
+          }
+    onSubmit(inputSearch); 
+    form.reset();
+  }
 
-const SearchBar = ({onSubmit }) => {
   return (
-    <header>
-          <form onSubmit ={onSubmit }>
-    <input
-      type="text"
-      autoComplete="off"
-      autoFocus
-      placeholder="Search images and photos"
-    />
-    <button type="submit">Search</button>
+<header>
+    <form onSubmit ={handleOnSubmit}>
+       <input type="text" autoComplete="off" autoFocus  placeholder="Search images and photos" name="search"/>
+       <button type="submit">Search</button>
+       <Toaster position="top-right" reverseOrder={false}/>
   </form>
 </header>
 
